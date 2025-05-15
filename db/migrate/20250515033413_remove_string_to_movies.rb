e2ea00cfe78a60075a6e8ce74e32fee290157b36
Tsering Lama <tseriing.boksha@gmail.com>
2025-05-15 22:00:25 +0900

    banner added

---
+++ db/migrate/20250515033413_remove_string_to_movies.rb
@@ -0,0 +1,4 @@
+class RemoveStringToMovies < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
---
+++ db/migrate/20250515040010_rename_poster_to_poster_url_in_movies.rb
@@ -0,0 +1,4 @@
+class RenamePosterToPosterUrlInMovies < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
