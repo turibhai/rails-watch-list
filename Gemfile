95312bed8a2331704dc74384d0b32afb79a55a3c
Tsering Lama <tseriing.boksha@gmail.com>
2025-05-16 12:53:00 +0900

    added photo column

--- Gemfile
+++ Gemfile
@@ -72,4 +72,5 @@ gem "autoprefixer-rails"
 gem "font-awesome-sass", "~> 6.1"
 gem "simple_form"
 gem "sassc-rails"
+gem "cloudinary"
  
--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,7 @@ class List < ApplicationRecord
     has_many :bookmarks, dependent: :destroy
     has_many :movies, through: :bookmarks
     validates :name, presence: true, uniqueness: true
+     has_one_attached :photo
    
     
     
