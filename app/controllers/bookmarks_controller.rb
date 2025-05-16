e3eada10774c10dcdc196bf79f079332675a99b0
Tsering Lama <tseriing.boksha@gmail.com>
2025-05-16 16:30:18 +0900

    debugging

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,7 +9,7 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@bookmark.list)
+      redirect_to list_path(@list)
     else
       render 'new', status: :unprocessable_entity
     end
